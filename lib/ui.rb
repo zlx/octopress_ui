5fd890ebfc61de59e7267d484e85ebc71acb9287
soffolk <lianxin.zhu@easyread.ly>
2013-03-28 23:25:08 +0800

    make work

--- lib/ui.rb
+++ lib/ui.rb
@@ -1,13 +1,26 @@
 require 'rubygems'
 require 'sinatra'
+require 'sanitize'
+require 'ruby-pinyin'
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  File.open("#{params[:title]}", 'w') do |f|
-    f.write params[:content]
+  return 'error' if params[:title] == "" || params[:content] == ""
+  title = PinYin.permlink(params[:title])
+  File.open("#{Time.now.strftime('%Y-%m-%d')}-#{title}.markdown", 'w') do |file|
+    file.puts "---"
+    file.puts "layout: post"
+    file.puts "title: #{params[:title]}"
+    file.puts "---"
+    file.puts ""
+    
+    lines = Sanitize.clean(params[:content], elements: ['br']).split("<br>")
+    lines.each do |line|
+      file.puts line
+    end
   end
-  'update success'
+  "update_success"
 end
