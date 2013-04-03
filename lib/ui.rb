0808116b07fe3b8f9f7595c6005c0548f521c1f2
soffolk <lianxin.zhu@easyread.ly>
2013-04-03 09:38:26 +0800

    use sinatra::base and add config

--- lib/ui.rb
+++ lib/ui.rb
@@ -1,26 +1,36 @@
-require 'rubygems'
-require 'sinatra'
-require 'sanitize'
+require 'sinatra/base'
 require 'ruby-pinyin'
 
-get '/' do
-  erb :index
-end
+$root = ::File.dirname(__FILE__)
 
-post '/' do
-  return 'error' if params[:title] == "" || params[:content] == ""
-  title = PinYin.permlink(params[:title])
-  File.open("#{Time.now.strftime('%Y-%m-%d')}-#{title}.markdown", 'w') do |file|
-    file.puts "---"
-    file.puts "layout: post"
-    file.puts "title: #{params[:title]}"
-    file.puts "---"
-    file.puts ""
-    
-    lines = Sanitize.clean(params[:content], elements: ['br']).split("<br>")
-    lines.each do |line|
-      file.puts line
-    end
+class UIServer < Sinatra::Base
+
+  get '/admin' do
+    erb :index
   end
-  "update_success"
+
+  post '/admin' do
+    return 'error' if params[:title] == "" || params[:content] == ""
+    title = PinYin.permlink(params[:title])
+    filename = File.join($root, 'source', '_posts', "#{Time.now.strftime('%Y-%m-%d')}-#{title}.markdown")
+    FileUtils.touch(filename) unless File.exists?(filename)
+    File.open(filename, 'w') do |file|
+      file.puts "---"
+      file.puts "layout: post"
+      file.puts "title: #{params[:title]}"
+      file.puts "---"
+      file.puts ""
+
+      lines = Sanitize.clean(params[:content], elements: ['br']).split("<br>")
+      lines.each do |line|
+        file.puts line
+      end
+    end
+    p system('git add .')
+    p system('rake generate')
+    p system('rake "deploy"')
+    p system("git commit -m 'add post #{params[:title]}'")
+    p system("git push origin source")
+    "update_success"
+  end
 end
